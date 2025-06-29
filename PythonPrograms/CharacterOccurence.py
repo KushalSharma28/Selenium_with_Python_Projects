80f9c02317b79311f28cf6c14784a6a903fc18da
KushalSharma28 <kushalsharma28499@gmail.com>
2025-06-29 19:51:35 +0530

    New Files Added

---
+++ PythonPrograms/CharacterOccurence.py
@@ -0,0 +1,31 @@
+# def calculate_concurrence(input_string):
+#     char_counts = {}
+#
+#     for c in input_string:
+#         char_counts[c] = char_counts.get(c, 0) + 1
+#
+#     return char_counts
+#
+# if __name__ == "__main__":
+#     input_string = "This is a test string"
+#     char_counts = calculate_concurrence(input_string)
+#
+#     print("Character Concurrence in the string:")
+#     for char, count in char_counts.items():
+#         print(f"Character '{char}': {count}")
+
+def main():
+    input_string = input("Enter a string: ")
+    char_count_map = {}
+
+    for c in input_string:
+        char_count_map[c] = char_count_map.get(c, 0) + 1
+
+    print("Character Concurrence:")
+    for key in char_count_map:
+        print(f"{key}: {char_count_map[key]}")
+
+if __name__ == "__main__":
+    main()
+
+
